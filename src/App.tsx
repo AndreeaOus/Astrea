31ad0b8821f4b9dc69a59e8ac49ace87675e6644
AndreeaOus <andreea_ous@yahoo.com>
2023-07-19 21:25:48 +0300

    Added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,68 @@
 function App() {
   return (
     <>
-      
+      <header>
+
+        <div>
+            <ul>
+                <a className="text-3xl font-bold underline" href="">Home</a>
+                <a href="">Reviews</a>
+                <a href="">Articles</a>
+            </ul>
+            <img src="" alt="" />
+            <ul>
+                <a href="">Blog</a>
+                <a href="">Resources</a>
+                <a href="">About</a>
+            </ul>
+        </div>
+        <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <div>
+                <h2>Menu</h2>
+                <img src="" alt="" />
+                <ul>
+                    <a href="">Home</a>
+                    <a href="">Reviews</a>
+                    <a href="">Articles</a>
+                    <a href="">Blog</a>
+                    <a href="">Resources</a>
+                    <a href="">About</a>
+                </ul>
+                <img src="" alt="" />
+            </div>
+        </div>
+
+        <div>
+            <section>
+                <h2>Popular Posts</h2>
+                <article>
+                    <img src="" alt="" />
+                    <h3></h3>
+                    <h4></h4>
+                </article>
+                <article>
+                    <img src="" alt="" />
+                    <h3></h3>
+                    <h4></h4>
+                </article>
+                <article>
+                    <img src="" alt="" />
+                    <h3></h3>
+                    <h4></h4>
+                </article>
+                <article>
+                    <img src="" alt="" />
+                    <h3></h3>
+                    <h4></h4>
+                </article>
+            </section>
+            <aside>
+
+            </aside>
+        </div>
+      </header>
     </>
   )
 }
